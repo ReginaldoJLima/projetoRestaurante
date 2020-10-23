b03e160f764c2af59979d9c942eb9b7eea51fe26
SemiramisCavalcanti <webdesigncavalcanti@gmail.com>
2020-10-23 07:23:15 -0300

    Inclusao das paginas inicial e fale conosco

---
+++ paginas/faleConosco.php
@@ -0,0 +1,50 @@
+<body>
+    <div class="container text-center">
+        <img src="public/img/faleConosco.jpg" alt="">  
+        <h1>Entre em contato com a gente</h1>
+    </div>
+    <!-- inicio do formulario -->
+    <div class="container">
+
+        <form>
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Assunto</label>
+                <select class="form-control" id="exampleFormControlSelect1" placeholder="Assunto">
+                    <option>Elogios</option>
+                    <option>Críticas/ Oportunidades de Melhorias</option>
+                    <option>Dúvidas</option>
+                    <option>Sugestões</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Nome Completo</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nome">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Email</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@examplo.com">
+            </div>
+            <label for="exampleFormControlSelect1">Assunto</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+                <option>Outback Shopping Guararapes</option>
+                <option>Outback Shopping Recife</option>
+                <option>Outback Shopping Rio Mar</option>
+                <option>Outback Patteo Shopping</option>
+            </select>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Mensagem</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <form>
+                    <div class="form-group row">
+                        <div class="col"><input type="file" class="form-control-file" id="exampleFormControlFile1"></div>
+                        <div class="col"><label for="exampleFormControlFile1">Anexar Arquivo (PNG, JPG, JPEG, PDF, BMP, GIF até 1MB)</label></div> 
+                    </div>
+                </form>
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+
+        </form>
+        
+    </div>
+
+</body>
