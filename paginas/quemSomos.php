ed14b61d70ef00c06a10b2cc1cda043a8920086c
jonasjunior00 <jonas_junior2009@hotmail.com>
2020-10-23 11:25:11 -0300

    diagramando

--- paginas/quemSomos.php
+++ paginas/quemSomos.php
@@ -24,7 +24,7 @@
     </div>
     <br>
     <div class="col-1"></div><img src="public/img/.jpg" alt="" class="img-fluid">
-  <br>
+    <br>
 
     <div class="col-5">
 
@@ -37,82 +37,111 @@
     <br>
   </div>
   <hr>
-  <div class="container-fluid"></div><img src="public/img/corona1.jpg" alt="" class="img-fluid rounded">
+
   <hr>
   <div class="container-fluid text-center">
+    <div class="row">
+      <div class="col-5">
+        <h3>Expansão da Rede</h3>
+        <br>
+        <p>O Outback é uma das maiores empresas de casual dining do mundo. Somos uma rede própria (não somos uma franquia) e cada uma de nossas unidades é liderada por um sócio-proprietário. Venha empreender com a gente! Cadastre o seu currículo em <strong>CARREIRAS!</strong>
 
-    <h3>Expansão da Rede</h3>
-    <br>
-    <p>O Outback é uma das maiores empresas de casual dining do mundo. Somos uma rede própria (não somos uma franquia) e cada uma de nossas unidades é liderada por um sócio-proprietário. Venha empreender com a gente! Cadastre o seu currículo em <strong>CARREIRAS!</strong>
+          Procuramos espaços de 600 a 700 m² com amplo estacionamento convenientemente disponível. Para gerenciar o restaurante: as oportunidades são avaliadas de forma independente.
 
-      Procuramos espaços de 600 a 700 m² com amplo estacionamento convenientemente disponível. Para gerenciar o restaurante: as oportunidades são avaliadas de forma independente.
+          Negociar Expansão: a avaliação de cidades e pontos comerciais é realizada internamente pela equipe especializada do Outback.</p>
+        <br>
+      </div>
+      <div class="col-5 container-fluid"><img src="public/img/corona1.jpg" alt="" class="img-fluid rounded"></div>
+    </div>
+    <hr>
 
-      Negociar Expansão: a avaliação de cidades e pontos comerciais é realizada internamente pela equipe especializada do Outback.</p>
-    <br>
     <h3>Carreiras</h3>
     <br>
     <p>O Outback é feito de gente apaixonada por momentos especiais, por servir bem e encantar clientes. Procuramos pessoas dedicadas, com perfil dinâmico e vontade de crescer. Veja as nossas oportunidades.</p>
+    <hr>
 
     <h3>Normas Relacionadas a COVID-19</h3>
     <br>
     <p>Neste momento, é fundamental reforçar os cuidados com a higiene das suas operações e do ambiente de trabalho e de atendimento ao público.
 
       Para garantir a segurança e também mostrar aos clientes que a outback está fazendo tudo o que pode para evitar a contaminação, é importante seguir os cuidados listados abaixo.</p>
-    <br>
-    <h5>Atendimento Seguro</h5>
-    <br>
-    <p>Além disso, outra prática importante é evitar ficar muito próximo ao cliente. Peça para os atendentes manterem cerca de um metro de distância dos consumidores.
+    <hr>
+    <div class="row">
+      <div class="col-6 border border-warning">
+        <h5>Atendimento Seguro</h5>
+        <br>
+        <p>Além disso, outra prática importante é evitar ficar muito próximo ao cliente. Peça para os atendentes manterem cerca de um metro de distância dos consumidores.
 
-      Para aqueles que pegam transporte público, providencie álcool em gel na entrada para que se higienizem antes de entrarem. Ou ainda peça para que, antes de fazer qualquer coisa, eles lavem muito bem as mãos.
+          Para aqueles que pegam transporte público, providencie álcool em gel na entrada para que se higienizem antes de entrarem. Ou ainda peça para que, antes de fazer qualquer coisa, eles lavem muito bem as mãos.
 
-      Por fim, é crucial que os profissionais estejam conscientes sobre a responsabilidade deles próprios para evitar que o vírus se espalhe.
+          Por fim, é crucial que os profissionais estejam conscientes sobre a responsabilidade deles próprios para evitar que o vírus se espalhe.
 
-      Peça que eles se afastem e fiquem em casa caso:
+          Peça que eles se afastem e fiquem em casa caso:
 
-      – Sentirem alguns dos sintomas causados pela COVID-19 (febre, tosse e dificuldade de respirar);
-      – Estiverem em contato com pessoas que estiveram fora do país;
-      – Estiverem em contato com alguém infectado ou que apresentou os sintomas. </p>
-    <br>
-    <h5>Segurança Alimentar</h5>
-    <br>
-    <p>O cuidado com a higiene no preparo dos alimentos já é algo básico sempre para quem trabalha no food service – seja na entrega ou em estabelecimentos. Contudo, nesse momento, é fundamental reforçar ainda mais essa questão.
+          – Sentirem alguns dos sintomas causados pela COVID-19 (febre, tosse e dificuldade de respirar);
+          – Estiverem em contato com pessoas que estiveram fora do país;
+          – Estiverem em contato com alguém infectado ou que apresentou os sintomas. </p>
 
-      Estamos falando de cuidados como, por exemplo:
+        <br>
+      </div>
+      <div class="col-6 border border-warning">
+        <h5>Segurança Alimentar</h5>
+        <br>
+        <p>O cuidado com a higiene no preparo dos alimentos já é algo básico sempre para quem trabalha no food service – seja na entrega ou em estabelecimentos. Contudo, nesse momento, é fundamental reforçar ainda mais essa questão.
 
-      – Usar luvas e máscaras;
-      – Limpar constantemente os equipamentos e utensílios compartilhados na cozinha;
-      – Desinfetar com mais frequência os equipamentos e as superfícies que entram em contato com o alimento;
-      – Manter a cozinha sempre bem ventilada;
-      – Aumentar a frequência com que os manipuladores de alimentos lavam as mãos – seguindo as boas práticas recomendadas para uma higienização correta das mãos.</p>
-    <br>
-    <h5>Limpeza dos Ambientes em Dia</h5>
-    <br>
-    <p>Novamente, estamos falando de algo que sempre deve ser feito: limpeza e organização em dia são fundamentais em negócios de food service. Porém, para evitar a propagação do novo coronavírus, é preciso reforçar esse cuidado.
+          Estamos falando de cuidados como, por exemplo:
 
-      Estas são algumas práticas importantes nesse sentido:
+          – Usar luvas e máscaras;
+          – Limpar constantemente os equipamentos e utensílios compartilhados na cozinha;
+          – Desinfetar com mais frequência os equipamentos e as superfícies que entram em contato com o alimento;
+          – Manter a cozinha sempre bem ventilada;
+          – Aumentar a frequência com que os manipuladores de alimentos lavam as mãos – seguindo as boas práticas recomendadas para uma higienização correta das mãos.</p>
+        <br>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-6">
+        <h5>Limpeza dos Ambientes em Dia</h5>
+        <br>
+        <p>Novamente, estamos falando de algo que sempre deve ser feito: limpeza e organização em dia são fundamentais em negócios de food service. Porém, para evitar a propagação do novo coronavírus, é preciso reforçar esse cuidado.
 
-      – Aumente a frequência de higienização (com álcool) das superfícies de contato compartilhado – mesas, maçanetas, suporte de guardanapos e canudos, cardápios, cadeiras, talheres do buffet etc.;
-      – Deixe recipientes com álcool em gel disponível para o público – na entrada do estabelecimento, em alguns lugares estratégicos dentro do ambiente (como perto do buffet ou no caixa) e na porta do banheiro.
-      – Limpe as mesas não só com um pano molhado, mas com álcool 70% para garantir que a superfície seja realmente desinfetada. Faça isso sempre antes de depois de os clientes sentarem.
-      – Crie um cronograma para, por exemplo, a cada 30 minutos um funcionário passar pano com álcool em maçanetas, cadeiras, portas, cardápios e outras superfícies e objetos compartilhados.
+          Estas são algumas práticas importantes nesse sentido:
+
+          – Aumente a frequência de higienização (com álcool) das superfícies de contato compartilhado – mesas, maçanetas, suporte de guardanapos e canudos, cardápios, cadeiras, talheres do buffet etc.;
+          – Deixe recipientes com álcool em gel disponível para o público – na entrada do estabelecimento, em alguns lugares estratégicos dentro do ambiente (como perto do buffet ou no caixa) e na porta do banheiro.
+          – Limpe as mesas não só com um pano molhado, mas com álcool 70% para garantir que a superfície seja realmente desinfetada. Faça isso sempre antes de depois de os clientes sentarem.
+          – Crie um cronograma para, por exemplo, a cada 30 minutos um funcionário passar pano com álcool em maçanetas, cadeiras, portas, cardápios e outras superfícies e objetos compartilhados.
 
 
 
 
-      Além disso, é importante manter os ambientes sempre bem arejados, com bastante circulação de ar. Se possível, caso não esteja muito calor, abra as janelas e desligue o ar-condicionado. De qualquer forma, é fundamental também assegurar a limpeza dos condicionadores de ar. </p>
-    <br>
-    <div class="container-fluid"></div><img src="public/img/corona3.jpg" alt="" class="img-fluid rounded">
+          Além disso, é importante manter os ambientes sempre bem arejados, com bastante circulação de ar. Se possível, caso não esteja muito calor, abra as janelas e desligue o ar-condicionado. De qualquer forma, é fundamental também assegurar a limpeza dos condicionadores de ar. </p>
+        <br>
+      </div>
+      <hr>
+      <div>
+        <div class="col-5"></div><img src="public/img/corona3.jpg" alt="" class="img-fluid rounded">
+      </div>
+      <hr>
+    </div>
     <hr>
     <h5>Proteção dos alimentos</h5>
     <br>
-    <div class="container-fluid"></div><img src="public/img/corona4.jpg" alt="" class="img-fluid rounded-circle">
-    <hr>
+    <div class="row">
+      <div class="col-5">
+        <div class="container-fluid"></div><img src="public/img/corona4.jpg" alt="" class="img-fluid rounded-circle">
+      </div>
 
-    <p>Se você trabalha em um negócio em que os alimentos geralmente ficam expostos – como buffet ou padaria, por exemplo –, deve ficar atento a essa questão. Para evitar que os produtos sejam contaminados, é aconselhável protegê-los.
+      <div class="col-6">
+        <p>Se você trabalha em um negócio em que os alimentos geralmente ficam expostos – como buffet ou padaria, por exemplo –, deve ficar atento a essa questão. Para evitar que os produtos sejam contaminados, é aconselhável protegê-los.
 
-      Para isso, você pode utilizar embalagens individuais, tampas ou mesmo um pano ou papel mais extenso para cobrir os itens.
+          Para isso, você pode utilizar embalagens individuais, tampas ou mesmo um pano ou papel mais extenso para cobrir os itens.
 
-      Em alguns casos, é aconselhável deixar um funcionário responsável por manusear os alimentos e entregar aos clientes. Em uma padaria, por exemplo, ao invés de expor os pães em uma cesta, você pode deixá-los na área interna do balcão, onde o cliente deverá solicitar o item que deseja.</p>
+          Em alguns casos, é aconselhável deixar um funcionário responsável por manusear os alimentos e entregar aos clientes. Em uma padaria, por exemplo, ao invés de expor os pães em uma cesta, você pode deixá-los na área interna do balcão, onde o cliente deverá solicitar o item que deseja.</p>
+        <hr>
+      </div>
+    </div>
     <hr>
     <h5>Número de mesas no estabelecimento reduzidas</h5>
     <br>
@@ -130,17 +159,22 @@
     <hr>
     <h5>Cuidado com as Operações de Delivery</h5>
     <br>
-    <div class="container-fluid"></div><img src="public/img/chopp.jpg" alt="" class="img-fluid rounded-circle">
-    <p>Quem trabalha com entrega de comida pode até sentir um aumento no movimento nesse contexto em que as pessoas estão com medo de comer fora de casa. Contudo, isso demanda um senso de responsabilidade ainda maior com os clientes que estão confiando em suas operações nesse momento.
+    <div class="row">
+      <div class="container-fluid col-4"><img src="public/img/chopp.jpg" alt="" class="img-fluid rounded"></div>
+      <div class="col-6">
+        <p>Quem trabalha com entrega de comida pode até sentir um aumento no movimento nesse contexto em que as pessoas estão com medo de comer fora de casa. Contudo, isso demanda um senso de responsabilidade ainda maior com os clientes que estão confiando em suas operações nesse momento.
 
-      Da mesma forma que é preciso tomar medidas preventivas em bares, restaurantes e lanchonetes – como as ações que indicamos acima –, também é crucial que os negócios que trabalham com delivery tomem os cuidados necessários.
+          Da mesma forma que é preciso tomar medidas preventivas em bares, restaurantes e lanchonetes – como as ações que indicamos acima –, também é crucial que os negócios que trabalham com delivery tomem os cuidados necessários.
 
-      Por isso, se você faz entrega de alimentos, precisa:
+          Por isso, se você faz entrega de alimentos, precisa:
 
-      – Reforçar a limpeza do ambiente onde os alimentos são manipulados;
-      – Atentar-se à higiene das pessoas e de utensílios, equipamentos e superfícies que entram em contato com o alimento;
-      – Verificar se os entregadores estão tomando as devidas medidas preventivas;
-      – Reforçar o cuidado com as embalagens. Por exemplo: colocar uma camada protetora extra para que as mãos do entregador e a superfície do transporte não entrem em contato com a embalagem do alimento.</p>
+          – Reforçar a limpeza do ambiente onde os alimentos são manipulados;
+          – Atentar-se à higiene das pessoas e de utensílios, equipamentos e superfícies que entram em contato com o alimento;
+          – Verificar se os entregadores estão tomando as devidas medidas preventivas;
+          – Reforçar o cuidado com as embalagens. Por exemplo: colocar uma camada protetora extra para que as mãos do entregador e a superfície do transporte não entrem em contato com a embalagem do alimento.</p>
+      </div>
+      <hr>
+    </div>
     <hr>
     <h5>3 ações que negócios de food service podem realizar para atender às necessidades dos consumidores em relação à prevenção da COVID-19</h5>
     <br>
